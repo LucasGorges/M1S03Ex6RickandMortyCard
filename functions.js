d14ded1af1d89f0f853f01d6877fc1c691b32750
Lucas Gorges <lucasgorges@hotmail.com>
2023-02-21 15:05:22 -0300

    first commit

--- functions.js
+++ functions.js
@@ -1,12 +1,13 @@
-async function search () {
-    const response = await fetch ('https://rickandmortyapi.com/api/character/657')
-    const data = await response.json()
-    console.log(data)
-    document.getElementById('name').innerHTML = `Name: ${data?.name}`
-    document.getElementById('status').innerHTML = `Status: ${data?.status}`
-    document.getElementById('gender').innerHTML = `Gender: ${data?.gender}`
-    document.getElementById('location').innerHTML = `Location: ${data?.location.name}`
-    document.getElementById('species').innerHTML = `Species: ${data?.species}`
-    
+async function search() {
+  const response = await fetch("https://api.github.com/users/LucasGorges");
+  const data = await response.json();
+  console.log(data);
+  document.getElementById("name").innerHTML = `Name: ${data?.name}`;
+  document.getElementById("status").innerHTML = `Status: ${data?.bio}`;
+  document.getElementById("twitter").innerHTML = `Twitter: ${data?.twitter_username}`;
+  document.getElementById("location").innerHTML = `Location: ${data?.location}`;
+  document.getElementById("repositorio").innerHTML = `Repository: ${data?.public_repos}`;
+  
 }
-search()
+
+search();
